125534b7dd3726e5cfb17b413e9f4416732e3ac1
Michael Benson <michael@viahero.com>
2018-12-07 04:24:31 -0500

    add blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: "projects#index", as: "index"
   get '/work', to: "projects#work", as: "work"
+  get '/work/safari', to: "projects#safari", as: "safari"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
